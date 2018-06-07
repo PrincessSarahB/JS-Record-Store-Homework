af70acb95abdbd7549f6da986b853a971d1eb7fd
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-07 20:03:42 +0100

    Record test and model set up and first test passing

---
+++ specs/Record_spec.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var Record = require('../Record.js');
+
+describe('Record', function(){
+  var record;
+
+  beforeEach(function(){
+    record = new Record("Stereophonics", "Word Gets Around","Rock", 10.00 )
+  });
+
+  it('should have an artist', function(){
+    assert.strictEqual(record.artist, "Stereophonics");
+  });
+
+  
+})
